98e29569f09424243f3e2cc7b32b9d8c9b2fe998
Bruno Eduardo Sousa <113060914+sous0049@users.noreply.github.com>
2024-03-05 09:54:26 -0500

    Header test

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <p>This is just a Test</p>
+    </main>
+  );
+}
