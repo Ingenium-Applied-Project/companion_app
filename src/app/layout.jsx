a8c420b993b8fa5969c9e2942e05485b41b402e4
Bruno Eduardo Sousa <113060914+sous0049@users.noreply.github.com>
2024-03-06 00:16:10 -0500

    Remove husky

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header.jsx";
+import Header from "../components/Header/Header.jsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
