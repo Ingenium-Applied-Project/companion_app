3a895d83886bc2ebcc23446d506b3fa6b32d21be
Bruno Eduardo Sousa <113060914+sous0049@users.noreply.github.com>
2024-03-07 16:12:41 -0500

    deployment: Test with netifly

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <p>This is just a Test</p>
+      <p>This is just a Test for CASM</p>
     </main>
   );
 }
