d18ff63843dc7cae3ba9b1b814001948867f7dfa
Bruno Eduardo Sousa <113060914+sous0049@users.noreply.github.com>
2024-03-11 17:32:05 -0400

    Change the structure of museum config

--- src/museumConfig.js
+++ src/museumConfig.js
@@ -1,12 +1,11 @@
-const config = [
-  {
-    name: 'CASM',
-    config: [
-      {
-        name: 'check list',
+const config = {
+  CASM: {
+    feature: {
+      checkList: {
         content: [
           {
             type: 'link',
+            for: 'location',
             settings: {
               screen: 'self', //or could be url: 'self'
               title: undefined,
@@ -18,6 +17,7 @@ const config = [
           },
           {
             type: 'text',
+            for: 'highlights',
             settings: {
               title: 'Highlights:',
               collapsable: false,
@@ -25,6 +25,7 @@ const config = [
           },
           {
             type: 'image gallery',
+            for: 'image-gallery',
             settings: {
               title: 'Image Gallery:',
               collapsable: false,
@@ -32,6 +33,7 @@ const config = [
           },
           {
             type: 'text',
+            for: 'history',
             settings: {
               title: 'History:',
               collapsable: false,
@@ -39,6 +41,7 @@ const config = [
           },
           {
             type: 'text',
+            for: 'provenance',
             settings: {
               title: 'Provenance:',
               collapsable: false,
@@ -46,6 +49,7 @@ const config = [
           },
           {
             type: 'text',
+            for: 'technical-information',
             settings: {
               title: 'Technical Information:',
               collapsable: false,
@@ -53,6 +57,7 @@ const config = [
           },
           {
             type: 'single media section',
+            for: 'airplane-model',
             settings: {
               title: undefined,
               collapsable: false,
@@ -60,18 +65,86 @@ const config = [
           },
         ],
       },
-      {},
-      {},
-    ],
+    },
   },
-  {
-    name: 'MUSEUM #2',
-    config: [{}, {}, {}],
-  },
-  {
-    name: 'MUSEUM #3',
-    config: [{}, {}, {}],
-  },
-];
+  MUSEUM2: {},
+  MUSEUM3: {},
+};
+
+// const config1 = [
+//   {
+//     name: 'CASM',
+//     config: [
+//       {
+//         name: 'check list',
+//         content: [
+//           {
+//             type: 'link',
+//             settings: {
+//               screen: 'self', //or could be url: 'self'
+//               title: undefined,
+//               icon: {
+//                 type: 'solid',
+//                 name: 'location-dot',
+//               },
+//             },
+//           },
+//           {
+//             type: 'text',
+//             settings: {
+//               title: 'Highlights:',
+//               collapsable: false,
+//             },
+//           },
+//           {
+//             type: 'image gallery',
+//             settings: {
+//               title: 'Image Gallery:',
+//               collapsable: false,
+//             },
+//           },
+//           {
+//             type: 'text',
+//             settings: {
+//               title: 'History:',
+//               collapsable: false,
+//             },
+//           },
+//           {
+//             type: 'text',
+//             settings: {
+//               title: 'Provenance:',
+//               collapsable: false,
+//             },
+//           },
+//           {
+//             type: 'text',
+//             settings: {
+//               title: 'Technical Information:',
+//               collapsable: false,
+//             },
+//           },
+//           {
+//             type: 'single media section',
+//             settings: {
+//               title: undefined,
+//               collapsable: false,
+//             },
+//           },
+//         ],
+//       },
+//       {},
+//       {},
+//     ],
+//   },
+//   {
+//     name: 'MUSEUM #2',
+//     config: [{}, {}, {}],
+//   },
+//   {
+//     name: 'MUSEUM #3',
+//     config: [{}, {}, {}],
+//   },
+// ];
 
 export default config;
