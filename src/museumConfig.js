d307c5ae18eb1567bf399fccd8e9d8d7c80991a9
Bruno Eduardo Sousa <113060914+sous0049@users.noreply.github.com>
2024-03-11 16:59:41 -0400

    Add check list configurations to config file

--- src/museumConfig.js
+++ src/museumConfig.js
@@ -1,7 +1,68 @@
 const config = [
   {
     name: 'CASM',
-    config: [{}, {}, {}],
+    config: [
+      {
+        name: 'check list',
+        content: [
+          {
+            type: 'link',
+            settings: {
+              screen: 'self', //or could be url: 'self'
+              title: undefined,
+              icon: {
+                type: 'solid',
+                name: 'location-dot',
+              },
+            },
+          },
+          {
+            type: 'text',
+            settings: {
+              title: 'Highlights:',
+              collapsable: false,
+            },
+          },
+          {
+            type: 'image gallery',
+            settings: {
+              title: 'Image Gallery:',
+              collapsable: false,
+            },
+          },
+          {
+            type: 'text',
+            settings: {
+              title: 'History:',
+              collapsable: false,
+            },
+          },
+          {
+            type: 'text',
+            settings: {
+              title: 'Provenance:',
+              collapsable: false,
+            },
+          },
+          {
+            type: 'text',
+            settings: {
+              title: 'Technical Information:',
+              collapsable: false,
+            },
+          },
+          {
+            type: 'single media section',
+            settings: {
+              title: undefined,
+              collapsable: false,
+            },
+          },
+        ],
+      },
+      {},
+      {},
+    ],
   },
   {
     name: 'MUSEUM #2',
