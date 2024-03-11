aeb28f1b590125f719b7f0ce386450ddb09bbac3
Bruno Eduardo Sousa <113060914+sous0049@users.noreply.github.com>
2024-03-11 16:45:29 -0400

    Create museum config file

---
+++ src/museumConfig.js
@@ -0,0 +1,16 @@
+const config = [
+  {
+    name: 'CASM',
+    config: [{}, {}, {}],
+  },
+  {
+    name: 'MUSEUM #2',
+    config: [{}, {}, {}],
+  },
+  {
+    name: 'MUSEUM #3',
+    config: [{}, {}, {}],
+  },
+];
+
+export default config;
