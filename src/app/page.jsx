675b4db35511e894254d8dea216a3c3180ef18a1
Bruno Eduardo Sousa <113060914+sous0049@users.noreply.github.com>
2024-03-12 16:03:17 -0400

    Create link to each route

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,24 @@
-import HeroImage from '@/components/HeroImage/HeroImage';
+import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <HeroImage />
-      <p>This is just a Test for CASM</p>
+      <h1>Home Page</h1>
+      <ul>
+        <li className={styles.link}>
+          <Link href="/hero-image">Hero Image</Link>
+        </li>
+        <li className={styles.link}>
+          <Link href="/check-list">Check List</Link>
+        </li>
+        <li className={styles.link}>
+          <Link href="/filename-generator">Filename Generator</Link>
+        </li>
+        <li className={styles.link}>
+          <Link href="/multiple-size-image">Multiple Size Image</Link>
+        </li>
+      </ul>
     </main>
   );
 }
