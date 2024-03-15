11fc055c1afc5e33183fb5d227ce9fbcbd9be702
Bruno Eduardo Sousa <113060914+sous0049@users.noreply.github.com>
2024-03-15 18:47:33 -0400

    Create function to fetch data for each screen

--- src/utils/api/fetchScreens.jsx
+++ src/utils/api/fetchScreens.jsx
@@ -6,6 +6,12 @@ const screens = async () => {
   const url = 'https://app.mytoursapp.com/api/v3/app/manifest';
   const screensArr = await fetchStqry(url);
   console.log(screensArr);
+  screensArr.screens.map(async (screen) => {
+    const screenUrl = `https://app.mytoursapp.com/api/v3/screens/${screen.id}/en/${screen.version}`;
+    const screenData = await fetchStqry(screenUrl);
+    console.log(screenData);
+    return screenData;
+  });
   return { screensArr };
 };
 
