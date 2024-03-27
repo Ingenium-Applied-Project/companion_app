ebca446ee498e5113c43134c42d894e0226a094c
serdarulutas <ulut0002@algonquinlive.com>
2024-03-27 15:42:40 -0400

    Update error description grammar

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -45,30 +45,34 @@ const HealthCheckErrorCodes = {
 };
 
 const HealthCheckErrorDescriptions = {
-  // {1 is generally the field name}
-  MUST_BE_BLANK: '{1}: Blank value is expected. Current value: {2}',
-  MUST_BE_A_VALUE: '{1}: Must have value of "{2}". Found "{3}"',
-  MUST_HAVE_A_VALUE: '{1}: Must have a value.',
-  MUST_HAVE_ONE_VALUE: '{1}: Expected one {2} item only, but found {3} items',
-  SECTION_NOT_FOUND: 'Section "{1}" not found! Expected {1} on index {2}',
+  // IMPORTANT: When you add a new value here, make sure you add it to HealthCheckErrorCodes as well.
 
-  TOO_SHORT: '{1}. The field is short. Expected minimum length: {2}',
+  MUST_BE_BLANK: '{1}: Expected no value but found "{2}".',
+  MUST_BE_A_VALUE: '{1}: Expected value to be "{2}", but found "{3}"."',
+  MUST_HAVE_A_VALUE: '{1}: A value is required.',
+  MUST_HAVE_ONE_VALUE:
+    '{1}: Expected exactly one {2} item, but found {3} items.',
+  SECTION_NOT_FOUND:
+    'Section "{1}" not found! Expected to find "{1}" at index {2}.',
+
+  TOO_SHORT: '{1}: The field is too short. Minimum expected length: {2}.',
   LOCATION_SELF_WRONG:
-    'Location pin should address the page itself. It is addressing another page.',
+    'The location pin should point to the current page, but it points to another page.',
+
   LOCATION_ICON_WRONG:
-    '{1}: Location pin icon is wrong. Expected {2}; found: {3}',
+    '{1}: Incorrect location pin icon. Expected "{2}", found "{3}".',
   LOCATION_ICON_STYLE:
-    '{1}: Location pin icon style is wrong. Expected {2}; found: {3}',
-  EMPTY_GALLERY_FOUND: 'Gallery is empty. ',
-  IMAGE_MISSING_PROPERTY: 'Missing {1} on image {2} {3}',
+    '{1}: Incorrect location pin icon style. Expected "{2}", found "{3}".',
+  EMPTY_GALLERY_FOUND: 'The gallery is empty.',
+  IMAGE_MISSING_PROPERTY: 'Image {2} {3} is missing the property "{1}".',
   IMAGE_DESCRIPTION_TOO_SHORT:
-    'Image description is too short. Expected minimum {1} but found {2} on image {3}{4}',
+    'Image description is too short. Minimum expected length: {1}, but found {2} for image {3}{4}.',
   IMAGE_EXTENSION_ERROR:
-    'Image extension is wrong. Expected "{1}" extension. Found "{2}" extension on image {3}',
+    'Incorrect image extension for image {3}. Expected "{1}", found "{2}".',
   DUPLICATE_CONTENT_MULTI:
-    'Duplicate Content on screen {1}: Check {2} for languages {3} and {4}.',
+    'Duplicate content detected on screen {1}. Check {2} for languages {3} and {4}.',
   DUPLICATE_IMAGE_DESCRIPTION_MULTI:
-    'Duplicate image descriptions on screen {1}: Check image {2} for languages {3} and {4}.',
+    'Duplicate image descriptions detected on screen {1}. Check image {2} for languages {3} and {4}.',
 };
 
 const HealthCheckSeverity = {
